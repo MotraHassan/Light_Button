e972bacfa8f72fa87638e14866155560db27d7f7
MotraHassan <133110048+MotraHassan@users.noreply.github.com>
2023-06-01 18:49:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
+
+btn1.addEventListener('click', ()=>{
+    btn1.classList.toggle('add')
+})
+btn2.addEventListener('click', ()=>{
+    btn2.classList.toggle('add')
+})
+btn3.addEventListener('click', ()=>{
+    btn3.classList.toggle('add')
+})
+btn4.addEventListener('click', ()=>{
+    btn4.classList.toggle('add')
+})
